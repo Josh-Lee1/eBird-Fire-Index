473d305fff583b8a2b9bc6af18ea9e1e6a5abd71
wcornwell <wcornwell@gmail.com>
2020-06-25 14:03:06 +1000

    changing read in problems

--- fire_matching_function.R
+++ fire_matching_function.R
@@ -4,23 +4,23 @@
 
 read_and_process_raw_ebird <- function(filename,fireshape=fireshape){
   #system("less -n1 RawData/ebd_AU_relApr-2020.txt 2>&1", intern = FALSE, show.output.on.console = TRUE)
-  baa<- read.delim(paste0("RawData/",filename), header = TRUE, sep="\t")
+  baa <- read_delim(filename, "\t", 
+                    escape_double = FALSE, trim_ws = TRUE)
+  colnames(baa)<-gsub(" ",".",colnames(baa))
   baatrimmed<- baa %>% 
     filter(ALL.SPECIES.REPORTED==1, EFFORT.DISTANCE.KM<10, DURATION.MINUTES<300, DURATION.MINUTES>10) %>% 
     dplyr::select(SAMPLING.EVENT.IDENTIFIER, LOCALITY.ID, LATITUDE, LONGITUDE, OBSERVATION.DATE) %>% 
     distinct()
-  
+  baatrimmed<-slice_sample(baatrimmed,n = 5)
   ebird_points <- st_as_sf(baatrimmed, coords=c("LONGITUDE", "LATITUDE"), crs=st_crs(fireshape))
   pointsinout<-st_join(ebird_points, fireshape, join = st_within)
   saveRDS(pointsinout,file = paste0("filteredData/",filename, ".rds"))
 }
 
 
-list.files
-
+all_files<-list.files("Rawdatatest",full.names = TRUE)
 
 fireshape_current<- st_read("National_Indicative_Aggregated_Fire_Extent_Dataset_v20200211/National_Indicative_Aggregated_Fire_Extent_Dataset_v20200211.shp")
-read_and_process_raw_ebird(filename="baa.csv",fireshape=fireshape_current)
-
-
-
+for(i in 1:length(all_files)){
+  read_and_process_raw_ebird(filename=all_files[i],fireshape=fireshape_current)
+}
