590cec9b91a9b7ed7fe864f6293877021512a453
Will Cornwell <wcornwell@gmail.com>
2020-07-13 15:12:09 +1000

    making into a function

--- corey_temp_script.R
+++ corey_temp_script.R
@@ -1,23 +1,21 @@
 library(tidyverse)
-
+library(lubridate)
 setwd("rawspeciesdata/")
 
-
 read_dat_function <- function(file_name) {
-  
   dat <- readRDS(file_name)
-  
+  dat$OBSERVATION.DATE<-ymd(dat$OBSERVATION.DATE)
   return(dat)
 }
 
 files <- list.files("../rawspeciesdata/")
 
 data <- lapply(files, read_dat_function)
-
 data_df <- do.call(rbind, data)
+data_df$LATITUDE<-as.numeric(data_df$LATITUDE)
+data_df<-filter(data_df,LATITUDE < -25)
 
 length(unique(data_df$SAMPLING.EVENT.IDENTIFIER))
-
 length(unique(data_df$COMMON.NAME))
 
 species_count <- data_df %>%
@@ -25,19 +23,41 @@ species_count <- data_df %>%
   summarize(N=n())
 
 
-sl <- data_df %>%
-  dplyr::filter(COMMON.NAME == "Superb Lyrebird") %>%
-  mutate(present=1)
+process<-function(species_name,data_df=data_df){
+  sl <- data_df %>%
+    dplyr::filter(COMMON.NAME == species_name) %>%
+    mutate(present=1)
+  
+  sl_lists <- sl %>%
+    dplyr::select(SAMPLING.EVENT.IDENTIFIER) %>%
+    distinct()
+  
+  lists_without <- data_df %>%
+    dplyr::filter(! SAMPLING.EVENT.IDENTIFIER %in% sl_lists$SAMPLING.EVENT.IDENTIFIER) %>%
+    dplyr::select(SAMPLING.EVENT.IDENTIFIER,OBSERVATION.DATE) %>% 
+    distinct() %>%
+    mutate(present=0)
+  
+  final_sl_dat <- sl %>%
+    bind_rows(lists_without)
+  
+  final_sl_dat$before.after<-final_sl_dat$OBSERVATION.DATE>ymd("2020-01-10")
+  
+  final_sl_dat %>%
+    group_by(before.after) %>%
+    summarize(percent_observed=sum(present)/n())->out
+    return(out)
+}
 
-sl_lists <- sl %>%
-  dplyr::select(SAMPLING.EVENT.IDENTIFIER) %>%
-  distinct()
+data_df %>%
+  group_by(COMMON.NAME) %>%
+  count(num.obs=n()) %>%
+  filter(num.obs>500)->a
 
-lists_without <- data_df %>%
-  dplyr::filter(! SAMPLING.EVENT.IDENTIFIER %in% sl_lists$SAMPLING.EVENT.IDENTIFIER) %>%
-  dplyr::select(SAMPLING.EVENT.IDENTIFIER) %>% 
-  distinct() %>%
-  mutate(present=0)
+setNames(as.list(a$COMMON.NAME),a$COMMON.NAME) %>% #need this weird trick to keep the species names
+  map_df(process,data_df,.id="var") -> out
 
-final_sl_dat <- sl %>%
-  bind_rows(lists_without)
+out %>%
+  spread(key="before.after",value="percent_observed") %>%
+  mutate(percentage_drop=(`FALSE`-`TRUE`)) %>%
+  arrange(desc(percentage_drop))
