f936b3a455723ca66563555dea6501bc10a74385
Corey Callaghan <callaghan.corey.t@gmail.com>
2020-07-22 07:28:54 +1000

    corey work

--- loop_for_models.R
+++ loop_for_models.R
@@ -2,7 +2,6 @@
 library(tidyverse)
 library(mgcv)
 
-setwd("model_objects/")
 
 mod<- readRDS("Australian_Magpie.RDS")
 datsum<- summary(mod)
@@ -12,25 +11,31 @@ devi<- mod$deviance
 wgoods<- data.frame(coeff, se, devi)
 tibble(species=file_name, coeff=coeff[2], se=se[2])
   
-all_files <- list.files("../model_objects/")
+all_files <- list.files("model_objects/")
 
   
-setwd("..")
+
   
   extractingcoeffse_function <- function(file_name) {
-    data <- readRDS(file_name)
+    data <- readRDS(paste0("model_objects/", file_name))
     datsum<- summary(data)
-    coeff<- data$coefficients
+    #coeff<- data$coefficients
     se<- datsum$se
-    deviance<- data$deviance
-    wgoods<- data.frame(coeff, se, deviance) 
+    deviance<- datsum$dev.expl
+    n<-datsum$n
+    wgoods<- as.data.frame(summary(data)$p.tab) %>%
+      rownames_to_column(var="Term") %>%
+      mutate(deviance=deviance) %>%
+      mutate(N=n) %>%
+      mutate(converged=data$converged) %>%
+      mutate(species=gsub(".RDS", "", file_name))
     
-    saveRDS(wgoods, file = paste0("models_extracted/", gsub("model_objects/", "",file_name), ".RDS"))
+    #saveRDS(wgoods, file = paste0("models_extracted/", gsub("model_objects/", "",file_name), ".RDS"))
     
-    return(tibble(species=file_name, coeff=coeff[2], se=se[2]))
+    return(wgoods)
   }
   
-  bird_response_df <- map_df(all_files, extractingcoeffse_function)
+  bird_response_df <- map_df(all_files[1:5], extractingcoeffse_function)
   
  
   # want the function to: read file, extract bits, make df, save it in another folder. 
