1b6a07f384daa4ed926fa8c1fae3dd06e4eb4995
Josh Lee <josh.lee2415@gmail.com>
2020-07-24 15:25:21 +1000

    making plots

--- trait.R
+++ trait.R
@@ -1,4 +1,5 @@
 library(dplyr)
+library(ggplot2)
 
 traits<- read.csv("trait_data.csv")
 
@@ -11,6 +12,29 @@ foodtraits<- traits %>%
   rename(species_name = `gsub(" ", "_", foodtraits$species)`) %>% 
   filter(species_name %in% fire_coefs$species) %>% 
   left_join(fire_coefs, foodtraits, by = "species_name")
+
+
+
+values<- filter(foodtraits, Term == "before.afterBefore")
+
+boxplot(Estimate ~ X165_Food_Seeds_10, data = values)
+boxplot(Estimate ~ X164_Food_Nectar_or_pollen_10, data = values)
+boxplot(Estimate ~ X166_Food_Foliage_or_herbs_10, data = values)
+boxplot(Estimate ~ X173_Food_fish_or_invertebrates_Inland_waters_10, data = values)
+boxplot(Estimate ~ X163_Food_Fruit_10, data = values)
+boxplot(Estimate ~ X167_Food_Corms_or_tubers, data = values)
+boxplot(Estimate ~ X169_Food_Terrestrial_vertebrates_10, data = values)
+boxplot(Estimate ~ X170_Food_Carrion_10, data = values)
+boxplot(Estimate ~ X171_Food_Intertidal_invertebrates_10, data = values)
+boxplot(Estimate ~ X172_Food_Fish_or_invertebrates_Marine_10, data = values)
+
+
+
+
+
+
+
+boxplot(Estimate ~ X165_Food_Seeds_10, data = foodtraits)
 
 
 
