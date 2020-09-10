083ce8d846b1257e2ff4824fd7df6383a3b34e82
Will Cornwell <wcornwell@gmail.com>
2020-09-10 10:30:35 +1000

    severity_and_traits

--- R/phesam_and_ebird.R
+++ R/phesam_and_ebird.R
@@ -1,4 +1,5 @@
 library(raster)
+library(lubridate)
 
 setwd("~/Documents/eBird-Fire-Index/")
 
@@ -26,11 +27,11 @@ ggplot(final_sl_dat,aes(x=COMMON.NAME,y=sev))+coord_flip()+geom_point()
 
 z<-group_by(final_sl_dat,COMMON.NAME) %>%
   summarize(n=n()) %>%
-  filter(n>75)
+  filter(n>50)
 
 m<-group_by(final_sl_dat,COMMON.NAME)%>%
   filter(sev >0) %>%
-  summarize(median.sev=median(sev))
+  summarize(median.sev=median(sev,na.rm=T))
 
 final_sl_dat %>%
   filter(COMMON.NAME %in% z$COMMON.NAME) %>%
@@ -39,4 +40,33 @@ final_sl_dat %>%
 ggplot(aes(x=reorder(COMMON.NAME, -median.sev),y=sev))+coord_flip()+geom_boxplot()+
   ylab("Average severity of area around checklist (larger number=more severe)")+
   xlab("")
+
+valuesfin$foodspec
+
+
+
+
+vf<-valuesfin %>%
+  mutate(feeding_specialisation=ifelse(foodspec>1,"generalist","specialist"))
+
+bird_response_df %>%
+  filter(Term=="before.afterBefore") %>%
+  mutate(COMMON.NAME=gsub("_"," ",species)) %>%
+  left_join(m) %>%
+  left_join(vf)->oo
+
+
+write_csv(oo,"processed_data/severity_data_and_traits.csv")
+
+library(readr)
+library(plotly)
+oo<-read_csv("processed_data/severity_data_and_traits.csv")
+z<-ggplot(oo,aes(x=median.sev,y=-1*Estimate,label=COMMON.NAME,))+
+  geom_point(aes(col=feeding_specialisation))+geom_smooth(method="lm")+ylab("modeled response to fire")+
+  geom_hline(yintercept = 0,linetype="dashed")+xlab("Median fire severity in post-fire observations")+
+  theme_classic()
+
+ggplotly(z)
+
+
 
